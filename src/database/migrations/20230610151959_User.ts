9dc66f0e82379441172e3f70238f9126af450ef2
diegomarcelino92 <ddmarcelino2014@gmail.com>
2023-09-17 12:12:15 -0300

    fix: limit columns db

--- src/database/migrations/20230610151959_User.ts
+++ src/database/migrations/20230610151959_User.ts
@@ -4,8 +4,8 @@ import { users } from '../database.tables'
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(users, (t) => {
     t.increments('id').primary()
-    t.text('name')
-    t.text('surname')
+    t.string('name', 30)
+    t.string('surname', 30)
     t.dateTime('birthdate')
     t.timestamp('created_at').defaultTo(knex.fn.now())
     t.timestamp('updated_at').defaultTo(knex.fn.now())
--- src/database/migrations/20230617135529_Genres.ts
+++ src/database/migrations/20230617135529_Genres.ts
@@ -4,8 +4,8 @@ import { genres } from '../database.tables'
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable(genres, (t) => {
     t.increments('id').primary()
-    t.string('name')
-    t.text('description')
+    t.string('name', 50)
+    t.string('description', 255)
     t.timestamp('created_at').defaultTo(knex.fn.now())
     t.timestamp('updated_at').defaultTo(knex.fn.now())
   })
