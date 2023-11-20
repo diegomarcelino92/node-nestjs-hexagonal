ace2f9e8f9e93a3838ff3e11f263fda89a02f520
diegomarcelino92 <ddmarcelino2014@gmail.com>
2023-11-20 12:36:10 -0300

    fix: table column interface

--- src/database/tables/users-genres.ts
+++ src/database/tables/users-genres.ts
@@ -3,6 +3,6 @@ import { BaseTable } from './base'
 export const usersGenres = 'users_genres'
 
 export interface UsersGenresTable extends BaseTable {
-  user_d: number
-  genre_d: number
+  user_id: number
+  genre_id: number
 }
